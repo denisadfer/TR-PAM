028c34c71a14ecc44f89849ad8fc91edb88f915d
grudasakti <saktikrida@gmail.com>
2022-04-18 00:23:38 +0700

    Layout profile page, fix warning

--- app/src/main/java/com/dnx/trpam/HomeFragment.java
+++ app/src/main/java/com/dnx/trpam/HomeFragment.java
@@ -1,11 +1,9 @@
 package com.dnx.trpam;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,8 +13,7 @@ import androidx.fragment.app.Fragment;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class HomeFragment extends Fragment implements View.OnClickListener{
-    Button btnLogout;
+public class HomeFragment extends Fragment{
     TextView TxtName;
     private FirebaseUser firebaseUser;
 
@@ -25,7 +22,6 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        btnLogout = (Button) view.findViewById(R.id.logout_btn);
         TxtName = view.findViewById(R.id.textName);
 
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
@@ -36,15 +32,6 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
             TxtName.setText("Data Name is empty");
         }
 
-        btnLogout.setOnClickListener(this);
-
         return view;
-    }
-
-    @Override
-    public void onClick(View v) {
-        FirebaseAuth.getInstance().signOut();
-        startActivity(new Intent(getActivity(), LoginActivity.class));
-        getActivity().finish();
     }
 }
--- app/src/main/java/com/dnx/trpam/ProfileFragment.java
+++ app/src/main/java/com/dnx/trpam/ProfileFragment.java
@@ -1,20 +1,52 @@
 package com.dnx.trpam;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class ProfileFragment extends Fragment {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class ProfileFragment extends Fragment implements View.OnClickListener {
+    TextView txtName,txtEmail;
+    Button btnLogout;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        txtName = view.findViewById(R.id.profile_name);
+        txtEmail = view.findViewById(R.id.profile_email);
+        btnLogout = (Button) view.findViewById(R.id.logout_btn);
+
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (firebaseUser != null) {
+            txtName.setText(firebaseUser.getDisplayName());
+            txtEmail.setText(firebaseUser.getEmail());
+        } else {
+            txtName.setText("Data Name is empty");
+            txtEmail.setText("Data Email is empty");
+        }
+
+        btnLogout.setOnClickListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getActivity(), LoginActivity.class));
+        getActivity().finish();
     }
 }
