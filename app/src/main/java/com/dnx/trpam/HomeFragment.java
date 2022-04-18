0e11c61866a31aac7212edd515583f79691b2c29
denisadfer <denisaditya80@gmail.com>
2022-04-18 10:42:19 +0700

    Add lib Picasso & CardView, change Home layout

--- app/src/main/java/com/dnx/trpam/HomeFragment.java
+++ app/src/main/java/com/dnx/trpam/HomeFragment.java
@@ -1,30 +1,36 @@
 package com.dnx.trpam;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.squareup.picasso.Picasso;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class HomeFragment extends Fragment{
     TextView TxtName;
+    ImageView cardImg;
     private FirebaseUser firebaseUser;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-
         TxtName = view.findViewById(R.id.textName);
-
+        cardImg = view.findViewById(R.id.cardImg);
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        Picasso.get().load("https://cdn.discordapp.com/attachments/865222321475158016/915966816046698526/unknown.png").fit().centerCrop().into(cardImg);
 
         if (firebaseUser!= null) {
             TxtName.setText("Welcome, "+firebaseUser.getDisplayName());
@@ -34,4 +40,6 @@ public class HomeFragment extends Fragment{
 
         return view;
     }
+
+
 }
