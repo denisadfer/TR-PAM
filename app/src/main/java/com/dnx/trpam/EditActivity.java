fae5cfb21fced756147bfc1644a2204c0e93c817
grudasakti <saktikrida@gmail.com>
2022-04-22 11:53:43 +0700

    get data to update profile

--- app/src/main/java/com/dnx/trpam/EditActivity.java
+++ app/src/main/java/com/dnx/trpam/EditActivity.java
@@ -3,11 +3,11 @@ package com.dnx.trpam;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -18,13 +18,10 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.HashMap;
-
 public class EditActivity extends AppCompatActivity {
     EditText editName, editUser, editPass, editPass2, editEmail, editPhone;
     Button btnSave;
-    FirebaseDatabase firebaseDatabase;
-    DatabaseReference databaseReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,27 +35,56 @@ public class EditActivity extends AppCompatActivity {
         btnSave = findViewById(R.id.save_btn);
 
         FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        String username = firebaseUser.getDisplayName();
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        databaseReference = firebaseDatabase.getReference().child(username);
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("DataUser");
 
-        getData();
-    }
-
-    private void getData() {
-        databaseReference.addValueEventListener(new ValueEventListener() {
+        databaseReference.child(firebaseUser.getDisplayName()).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                //getData from Firebase
+                if(snapshot.exists()){
+                    editName.setText(snapshot.child("name").getValue().toString());
+                    editEmail.setText(snapshot.child("email").getValue().toString());
+                    editUser.setText(snapshot.child("username").getValue().toString());
+                    editPhone.setText(snapshot.child("phone").getValue().toString());
+                    //editPass
 
+                    btnSave.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            String name = editName.getText().toString();
+                            String email = editEmail.getText().toString();
+                            String userName = editUser.getText().toString();
+                            String phone = editPhone.getText().toString();
+                            //String pass =
+                            //String pass2 =
+
+                            editData(name, email, userName, phone);
+                            Toast.makeText(getApplicationContext(),"Updated Successfully", Toast.LENGTH_LONG).show();
+                            finish();
+                        }
+                    });
+                }
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                // calling on cancelled method when we receive
-                // any error or we are not able to get the data.
                 Toast.makeText(getApplicationContext(), "Fail to get data.", Toast.LENGTH_SHORT).show();
             }
         });
+
+    }
+
+    public void editData(String name, String email, String userName, String phone){
+        if(!TextUtils.isEmpty(name))
+            FirebaseDatabase.getInstance().getReference("DataUser").child(userName).child("name").setValue(name);
+
+        if(!TextUtils.isEmpty(email))
+            FirebaseDatabase.getInstance().getReference("DataUser").child(userName).child("email").setValue(email);
+
+        if(!TextUtils.isEmpty(userName))
+            FirebaseDatabase.getInstance().getReference("DataUser").child(userName).child("username").setValue(userName);
+
+        if(!TextUtils.isEmpty(phone))
+            FirebaseDatabase.getInstance().getReference("DataUser").child(userName).child("phone").setValue(phone);
+
     }
 }
