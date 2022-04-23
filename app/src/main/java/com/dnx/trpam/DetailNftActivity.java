1866f1e794823ffe60d7878538ccec95fb30734d
DenJudy001 <popond0001@gmail.com>
2022-04-23 13:44:48 +0700

    add listing function

--- app/src/main/java/com/dnx/trpam/DetailNftActivity.java
+++ app/src/main/java/com/dnx/trpam/DetailNftActivity.java
@@ -3,13 +3,18 @@ package com.dnx.trpam;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -19,6 +24,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
+import java.util.HashMap;
+
+import in.shadowfax.proswipebutton.ProSwipeButton;
+
 public class DetailNftActivity extends AppCompatActivity {
 
     TextView nft_owner, nft_price, nft_token, nft_title;
@@ -27,12 +36,15 @@ public class DetailNftActivity extends AppCompatActivity {
     DatabaseReference dataref;
     private FirebaseUser firebaseUser;
     LinearLayout nft_linear;
+    Dialog mdialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_nft);
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        mdialog = new Dialog(this);
 
         nft_owner = findViewById(R.id.detail_nft_owner);
         nft_price = findViewById(R.id.detail_nft_price);
@@ -43,6 +55,7 @@ public class DetailNftActivity extends AppCompatActivity {
         nft_buy = findViewById(R.id.detail_nft_buy);
         nft_linear = findViewById(R.id.detail_nft_linearBtn);
         dataref = FirebaseDatabase.getInstance().getReference().child("Nft_Post");
+        NftPost nftAdd_price = new NftPost();
 
         String Nft_key = getIntent().getStringExtra("Nft_Post_key");
 
@@ -56,13 +69,18 @@ public class DetailNftActivity extends AppCompatActivity {
                     String token = snapshot.child("token").getValue().toString();
                     String img = snapshot.child("img").getValue().toString();
 
+                    nftAdd_price.setImg(img);
+                    nftAdd_price.setTitle(title);
+                    nftAdd_price.setToken(token);
+                    nftAdd_price.setOwner(owner);
+
                     Picasso.get().load(img).into(nft_img);
                     nft_owner.setText(owner);
                     nft_title.setText(title);
                     nft_price.setText(price);
                     nft_token.setText(token);
                     if (owner.equals(firebaseUser.getDisplayName())){
-                        nft_listing.setVisibility(View.GONE);
+                        nft_listing.setVisibility(View.VISIBLE);
                         nft_linear.setVisibility(View.VISIBLE);
                         nft_buy.setVisibility(View.GONE);
                     }
@@ -72,6 +90,56 @@ public class DetailNftActivity extends AppCompatActivity {
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        nft_listing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Button close_popup;
+                EditText popup_price;
+                ProSwipeButton swipe_listing;
+                mdialog.setContentView(R.layout.popup_listing);
+                popup_price = mdialog.findViewById(R.id.popup_price);
+                swipe_listing = mdialog.findViewById(R.id.swipe_listing);
+                close_popup = mdialog.findViewById(R.id.close_popup_btn);
+                mdialog.show();
+                close_popup.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        mdialog.dismiss();
+                    }
+                });
+                swipe_listing.setOnSwipeListener(new ProSwipeButton.OnSwipeListener() {
+                    @Override
+                    public void onSwipeConfirm() {
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                swipe_listing.showResultIcon(true);
+                                nftAdd_price.setPrice(Double.parseDouble(popup_price.getText().toString()));
+                                dataref.child(Nft_key).setValue(nftAdd_price).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        new Handler().postDelayed(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                mdialog.dismiss();
+                                                finish();
+                                                startActivity(getIntent());
+                                            }
+                                        },1000);
+
+                                    }
+                                });
+
+                            }
+                        },2000);
+
+
+                    }
+                });
 
             }
         });
