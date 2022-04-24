08f0aa01bdc0547ed01095cd969184a2b338cb04
denisadfer <denisaditya80@gmail.com>
2022-04-24 18:14:13 +0700

    Add splash screen

---
+++ app/src/main/java/com/dnx/trpam/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.dnx.trpam;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        }, 3000);
+    }
+}
